9075a287860fdfa7e8b846340391665ea4712fc2
ollie1400 <otlg123@gmail.com>
2017-05-06 01:26:34 +0100

    also to fit a second order polynomial

--- linearregression.m
+++ linearregression.m
@@ -1,20 +1,23 @@
 function linearregression
 
 % data
-x = 1:10;
+x = 1:100;
 m = length(x);
 y = 3 * x + rand(1, length(x));
+y = 5*x.*x + 2*x + 13 + rand(1, length(x));
+assignin('base','x',x);
+assignin('base','y',y);
 
 % model
-theta = rand(2,1);  % parameter vector, start random
-h = @(theta,x) theta(1) + theta(2) * x;  % the model to fit
-dh = @(theta,x) [ones(1,m); x];  % differental of the model. Element dh(i,j) = dh(theta,x_j)/dtheta_i
+theta = rand(3,1);  % parameter vector, start random
+h = @(theta,x) theta(1) + theta(2) * x + theta(3) * x .* x;  % the model to fit
+dh = @(theta,x) [ones(1,m); x; x.*x];  % differental of the model. Element dh(i,j) = dh(theta,x_j)/dtheta_i
 
 % do gradient descent
-alpha = 0.0001;  % learning rate
-n = 1000;
+alpha = 1e-12;  % learning rate
+n = 10000;
 lastJ = NaN;
-threshold = 0.01;
+threshold = 0.00001;
 converged = 0;
 Js = zeros(n,1);
 display('Starting gradient descent:');
@@ -27,7 +30,7 @@ for i=1:n
     
     if ~isnan(lastJ)
         pdiff = (J - lastJ) / J;
-        display(sprintf('Change by %+.2f%%', pdiff*100));
+        display(sprintf('(%i) Change by %+.2f%%', i, pdiff*100));
         if abs(pdiff) < threshold
             converged = 1;
             break;
@@ -42,6 +45,7 @@ semilogy(1:n,Js);
 xlabel('Iteration of gradient descent');
 ylabel('Cost function');
 grid on;
+display(theta);
 
 switch converged
     case 0
@@ -50,13 +54,11 @@ switch converged
         display('Converged!');
 end
 
-if converged
-    figure;
-    hold on;
-    plot(x,y,'o');
-    plot(x, h(theta,x));
-    legend('Data', 'Fit');
-end
+figure;
+hold on;
+plot(x,y,'o');
+plot(x, h(theta,x));
+legend('Data', 'Fit');
 
 end
 
